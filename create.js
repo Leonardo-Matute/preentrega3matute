7179f970aecfcc98fcb66214062b165830269dd3
Unai Bolivar <ubolivar@c3r1s3.42madrid.com>
2022-12-27 18:07:34 +0100

    should work but is not loading

--- create.js
+++ create.js
@@ -1,19 +1,64 @@
+// JSON Data
+
+const camas = [];
+let list;
+
+const camaOpciones = (list, camas) => {
+	if (list) {
+		camas.map((cama) => {
+			let li = `		
+			<li>
+			<a class="dropdown-item">
+			<div class="form-check">
+			<input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+			<label class="form-check-label" for="flexRadioDefault1">
+			${cama.nombre} $${cama.precio}
+			</label>
+			</div>
+			</a>
+			</li>
+			`;
+			let element = document.createElement('div');
+			element.innerHTML = li;
+			list.appendChild(element.firstChild);
+		});
+	} else {
+		console.error("list not found");
+	}
+};
+
+window.onload = () => {	
+	fetch('./datos.json')
+	.then(res => res.json())
+	.then(data => {
+		data.forEach((cama) => {
+			camas.push(cama);
+		})
+	})
+	.then(() => {
+		list = document.getElementById("seleccion-de-camas");
+		camaOpciones(list, camas);
+	})
+	.catch(error => console.error(error));
+}
+
+console.log(list);
 
 const guardarreserva = (clave, valor) => (localStorage.setItem(clave, valor))
 
 
 agregar.addEventListener('click', () => {
-const nuevareserva = [
-    {
-        nombre: document.querySelector('#nombre').value,
-        apellido: document.querySelector('#apellido').value,
-        mail: document.querySelector('#mail').value,
-        telefono: document.querySelector('#telefono').value
-    }
-]
-
-
-guardarreserva('cargaReserva', JSON.stringify(nuevareserva))
+	const nuevareserva = [
+		{
+			nombre: document.querySelector('#nombre').value,
+			apellido: document.querySelector('#apellido').value,
+			mail: document.querySelector('#mail').value,
+			telefono: document.querySelector('#telefono').value
+		}
+	]
+	
+	
+	guardarreserva('cargaReserva', JSON.stringify(nuevareserva))
 }
 )
 
