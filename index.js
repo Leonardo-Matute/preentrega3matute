3787cb6f2459df6ef071dc4a2df0053914376e8b
Leonardo <leonardo.matute@gmail.com>
2022-12-29 10:16:32 -0300

    preentrega3

--- index.js
+++ index.js
@@ -1,99 +1,70 @@
-let noche = JSON.stringify('.noche')
+let agregar = document.querySelector('#agregar');
 
+let noche = document.querySelector('.noche');
+
+fetch('./datos.json')
+  .then(response => response.json())
+  .then(data => {
+  });
 
 let mostrarreserva = (() => {
+  
+  let listadoreserva = JSON.parse(localStorage.getItem('reserva'));
 
-    let listadoreserva = JSON.parse(localStorage.getItem('reserva'))
 
-    for (let index = 0; index < listadoreserva
-    .length; index++) {
-        let div = document.createElement('div')
-        div.className = 'col s4'
-        div.innerHTML = `
-        <div class="card text-center">
+  for (let index = 0; index < listadoreserva.length; index++) {
+   
+    let div = document.createElement('div');
+    div.className = 'col s4';
+
+  
+    div.innerHTML = `
+      <div class="card text-center">
         <div class="card-header">
-            ${listadoreserva
-        [index].mail}
+          ${listadoreserva[index].email}
         </div>
         <div class="card-body">
-            <h5 class="card-title">${listadoreserva
-        [index].nombre}</h5>
-            <p class="card-text">${listadoreserva
-        [index].apellido}</p>
-            <a href="#" class="btn btn-danger">Ver reserva</a>
+          <h5 class="card-title">${listadoreserva[index].nombre}</h5>
+          <p class="card-text">${listadoreserva[index].apellido}</p>
+          <a href="#" class="btn btn-danger">Ver reserva</a>
         </div>
         <div class="card-footer text-muted">
-        ${listadoreserva
-    [index].telefono} - ${listadoreserva}
+          ${listadoreserva[index].telefono}
         </div>
-    </div>
-         `
-    noche.appendChild(div)
+      </div>
+    `;
 
-    }
+   
+    noche.appendChild(div);
+  }
+});
+
+let mostrarNoche = (() => {
+  
+  let menu = JSON.parse(localStorage.getItem('Noche'));
+
+  
+  for (let index = 0; index < menu.length; index++) {
+  
+    let div = document.createElement('div');
+    div.className = 'col s4';
+
+    div.innerHTML = `
+      <div class="card text-center">
+        <div class="card-header">
+          ${menu[index].email}
+        </div>
+        <div class="card-body">
+          <h5 class="card-title">${menu[index].nombre}</h5>
+          <p class="card-text">${menu[index].apellido}</p>
+          <a href="#" class="btn btn-danger">Ver reserva</a>
+        </div>
+        <div class="card-footer text-muted">
+          ${menu[index].telefono}
+        </div>
+      </div>
+    `;
+  }
 }
 )
-
-
-let mostrarNoche = (()=>{
-let menu = JSON.parse(localStorage.getItem ('Noche'))
-
-for (let index = 0; index < listadoreserva
-    .length; index++) {
-        let div = document.createElement('div')
-        div.className = 'col s4'
-        div.innerHTML = `
-        <div class="card text-center">
-        <div class="card-header">
-            ${listadoreserva
-        [index].mail}
-        </div>
-        <div class="card-body">
-            <h5 class="card-title">${listadoreserva
-        [index].nombre}</h5>
-            <p class="card-text">${listadoreserva
-        [index].apellido}</p>
-            <a href="#" class="btn btn-danger">Ver reserva</a>
-        </div>
-        <div class="card-footer text-muted">
-        ${listadoreserva
-    [index].telefono} - ${listadoreserva}
-        </div>
-    </div>
-         `
-
-    noche.appendChild(div)
-
-}
-
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
